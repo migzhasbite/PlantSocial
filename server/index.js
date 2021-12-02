fab6fd4d0f87b92019f2cb51edfb4b8fb7bce058
migzhasbite <migzhasbite@gmail.com>
2021-12-02 14:03:42 -0500

    Applied KNEX query builder CRUD logic for MySQL DB for users.

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express();
 const path = require("path");
 const morgan = require("morgan");
 
-// const usersRoutes = require("./routes/usersRoutes");
+const usersRoutes = require("./routes/usersRoutes");
 
 require("dotenv").config();
 const port = process.env.PORT || process.argv[2] || 8080;
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use("/users", usersRoutes);
+app.use("/users", usersRoutes);
 
 app.use(morgan("dev"));
 
---
+++ server/knexfile.js
@@ -0,0 +1,10 @@
+module.exports = {
+	client: "mysql",
+	connection: {
+		host: "127.0.0.1",
+		user: "root",
+		password: "Imperial!6307",
+		database: "mydb",
+		charset: "utf8",
+	},
+};
