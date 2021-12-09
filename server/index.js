010815270b167d3996a7d0558d7651f1a54d1fc7
migzhasbite <migzhasbite@gmail.com>
2021-12-08 19:14:42 -0500

    Added front-end functionality and styling. Reverted to using JSON to retrieve data from the backend.

--- server/index.js
+++ server/index.js
@@ -4,24 +4,41 @@ const app = express();
 const path = require("path");
 const morgan = require("morgan");
 
-const usersRoutes = require("./routes/usersRoutes");
 const postsRoutes = require("./routes/postsRoutes");
-const commentsRoutes = require("./routes/commentsRoutes");
 
 require("dotenv").config();
 const port = process.env.PORT || process.argv[2] || 8080;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/users", usersRoutes);
-app.use("/posts", postsRoutes);
-app.use("/comments", commentsRoutes);
-
+app.use("/static", express.static("public"));
 app.use(morgan("dev"));
+
+app.use("/posts", postsRoutes);
 
 app.listen(port, () => {
 	console.log(`Server Started on http://localhost:${port}`);
 	console.log("Press CTRL + C to stop server");
 });
+
+// // const usersRoutes = require("./routes/usersRoutes"); //Users for future build
+// const postsRoutes = require("./routes/postsRoutes");
+// // const commentsRoutes = require("./routes/commentsRoutes"); //Comments for future build
+
+// require("dotenv").config();
+// const port = process.env.PORT || process.argv[2] || 8080;
+
+// app.use(cors());
+// app.use(express.json());
+// app.use("/static", express.static("public"));
+
+// // app.use("/users", usersRoutes); //Users for future build
+// app.use("/posts", postsRoutes);
+// // app.use("/comments", commentsRoutes); //Comments route for future build
+
+// app.use(morgan("dev"));
+
+// app.listen(port, () => {
+// 	console.log(`Server Started on http://localhost:${port}`);
+// 	console.log("Press CTRL + C to stop server");
+// });
--- server/routes/postsRoutes.js
+++ server/routes/postsRoutes.js
@@ -1,57 +1,182 @@
 const express = require("express");
 const router = express.Router();
-const knex = require("../data/connection");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+// const knex = require("../data/connection");
+
+let postData = [];
+
+const getPostData = () => {
+	fs.readFile("./data/post-details.json", (err, data) => {
+		if (err) {
+			console.log(err);
+			return;
+		}
+		postData = JSON.parse(data);
+	});
+};
+
+getPostData();
+
+router.get("/", (_req, res) => {
+	res.json(postData);
+});
+
+router.get("/:id", (req, res) => {
+	const { id } = req.params;
+	const post = postData.find((post) => {
+		return post.id === id;
+	});
+	if (post) {
+		res.json(post);
+	} else {
+		res.status(404).send("Page not found.");
+	}
+});
+
+router.post("/", (req, res) => {
+	let posts = postData;
+	const { title, description, image } = req.body;
+	if (!(title && description && image)) {
+		res.status(400).send(`Please provide title and description.`);
+	}
+	const newPost = {
+		title,
+		description,
+		channel: "Miguel Esteves",
+		image,
+		views: "0",
+		likes: "0",
+		duration: "0:50",
+		post: "https://project-2-api.herokuapp.com/stream",
+		timestamp: new Date().getTime(),
+		comments: [],
+		id: uuidv4(),
+	};
+	posts.push(newPost);
+	fs.writeFile("./data/post-details.json", JSON.stringify(posts), (err) => {
+		if (err) {
+			res.status(500).send(
+				`Server encountered an unexpected condition that prevented
+			it from uploading post.`,
+				err
+			);
+		} else {
+			console.log("Successfully created new post.");
+			res.status(201).json(newPost);
+		}
+	});
+});
 
 router
-	.get("/:id", (req, res) => {
-		knex("posts")
-			.where({ id: req.params.id })
-			.then((user) => {
-				console.log(user), res.send(user);
-			})
-			.catch(() => {
-				res
-					.status(400)
-					.json({ message: `Error getting user ${req.params.id}` });
-			});
+	.post("/:id/comments", (req, res) => {
+		let posts = postData;
+		const { id } = req.params;
+		const { comment } = req.body;
+		const selectedPost = posts.find((post) => {
+			return post.id === id;
+		});
+		const newComment = {
+			commentID: uuidv4(),
+			name: "Miguel Esteves",
+			comment,
+			likes: "0",
+			timestamp: new Date().getTime(),
+		};
+		console.log(selectedPost);
+		selectedPost.comments.push(newComment);
+		fs.writeFile("./data/post-details.json", JSON.stringify(posts), (err) => {
+			if (err) {
+				console.log(err);
+			} else {
+				res.status(200).send(selectedPost.comments);
+			}
+		});
 	})
-	.post("/", (req, res) => {
-		knex("posts")
-			.insert(req.body)
-			.then((user) => res.status(201).json(user))
-			.catch(() => {
-				res
-					.status(400)
-					.json({ message: `Error creating user ${req.params.id}` });
-			});
-	})
-	.put(
-		("/:id",
-		(req, res) => {
-			knex("posts")
-				.where({ id: req.params.id })
-				.update(req.body)
-				.then(() => res.sendStatus(204))
-				.catch(() =>
-					res
-						.status(400)
-						.json({ message: `Error updating user ${req.params.id}` })
-				);
-		})
-	)
-	.delete(
-		("/:id",
-		(req, res) => {
-			knex("posts")
-				.where({ id: req.params.id })
-				.del()
-				.then(() => res.sendStatus(204))
-				.catch(() =>
-					res
-						.status(400)
-						.json({ message: `Error deleting user ${req.params.id}` })
-				);
-		})
-	);
+	.delete("/:id/comments/:commentId", (req, res) => {
+		let posts = postData;
+		const postId = req.params.id;
+		const commentId = req.params.commentId;
+		const selectedPost = posts.find((post) => {
+			return post.id === postId;
+		});
+		const deletedComment = selectedPost.comments.find((comment) => {
+			return comment.commentID === commentId;
+		});
+		if (deletedComment) {
+			selectedPost.comments.splice(
+				selectedPost.comments.indexOf(deletedComment),
+				1
+			);
+		}
+		fs.writeFile(
+			"./data/post-details.json",
+			JSON.stringify(postData),
+			(err) => {
+				if (err) {
+					console.log(err);
+				} else {
+					res.status(200).send(deletedComment);
+				}
+			}
+		);
+	});
 
 module.exports = router;
+
+/*
+ *** Contains routes with KNEXjs logic for future build
+ */
+// router
+// 	.get("/:id", (req, res) => {
+// 		knex("posts")
+// 			.where({ id: req.params.id })
+// 			.then((user) => {
+// 				console.log(user), res.send(user);
+// 			})
+// 			.catch(() => {
+// 				res
+// 					.status(400)
+// 					.json({ message: `Error getting user ${req.params.id}` });
+// 			});
+// 	})
+// 	.post("/", (req, res) => {
+// 		knex("posts")
+// 			.insert(req.body)
+// 			.then((user) => res.status(201).json(user))
+// 			.catch(() => {
+// 				res
+// 					.status(400)
+// 					.json({ message: `Error creating user ${req.params.id}` });
+// 			});
+// 	})
+// 	.put(
+// 		("/:id",
+// 		(req, res) => {
+// 			knex("posts")
+// 				.where({ id: req.params.id })
+// 				.update(req.body)
+// 				.then(() => res.sendStatus(204))
+// 				.catch(() =>
+// 					res
+// 						.status(400)
+// 						.json({ message: `Error updating user ${req.params.id}` })
+// 				);
+// 		})
+// 	)
+// 	.delete(
+// 		("/:id",
+// 		(req, res) => {
+// 			knex("posts")
+// 				.where({ id: req.params.id })
+// 				.del()
+// 				.then(() => res.sendStatus(204))
+// 				.catch(() =>
+// 					res
+// 						.status(400)
+// 						.json({ message: `Error deleting user ${req.params.id}` })
+// 				);
+// 		})
+// 	);
+
+// module.exports = router;
